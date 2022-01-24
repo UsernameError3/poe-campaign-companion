a3a26664e428c1ea75fd38dded3189ad8c236f35
Alex Fleming <alexfleming421@gmail.com>
2022-01-24 04:05:47 -0600

    Component Structure

---
+++ app/views/components/links/links.js
@@ -0,0 +1,53 @@
+
+// Links Button
+addBtn.addEventListener('click', () => {
+    const dirtyInput = document.getElementById('myInput').value;
+    api.links.addNewLink(dirtyInput).then(data => {
+        document.getElementById('myInput').value = null;
+        updateView();
+    });
+});
+
+document.getElementById('addBtn').addEventListener('click', () => {
+    const inputValue = document.getElementById('myInput').value;
+    if (inputValue) {
+        dbInstance.create({content: inputValue})
+        .then(result => {
+            document.getElementById('myInput').value = null;
+            updateView();
+        })
+    }
+})
+
+
+const dbInstance = remote.getGlobal('db');
+
+function createTodoItemView(content) {
+    const liNode = document.createElement('li');
+    // close button
+    const span = document.createElement("span");
+    span.className = 'close';
+    const txt = document.createTextNode("\u00D7");
+    span.appendChild(txt);
+
+    liNode.textContent = content;
+    liNode.appendChild(span);
+    return liNode;
+}
+
+function updateView() {
+    const todolistNode = document.getElementById('todolist');
+    todolistNode.innerHTML = '';
+
+    dbInstance.readAll()
+    .then(allTodolists => {
+        allTodolists.forEach(item => {
+            const liNode = createTodoItemView(item.content);
+            todolistNode.appendChild(liNode);
+        });
+    })
+}
+
+
+
+updateView();
